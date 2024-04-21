287a5d0326a6f6a91db2385b105c0511e18da6b1
Tijana Matić <matic.sr22.2022@uns.ac.rs>
2024-04-21 20:37:52 +0200

    Dodavanje hendlera za getGroup i addGroup

---
+++ projekat/handler/configGroupHandler.go
@@ -0,0 +1,59 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"projekat/model"
+	"projekat/service"
+
+	"github.com/gorilla/mux"
+)
+
+type ConfigGroupHandler struct {
+	service service.ConfigurationGrupService
+}
+
+func NewConfigGroupHandler(service service.ConfigurationGrupService) ConfigGroupHandler {
+	return ConfigGroupHandler{
+		service: service,
+	}
+}
+
+func (c ConfigGroupHandler) GetGroup(w http.ResponseWriter, r *http.Request) {
+	name := mux.Vars(r)["name"]
+	version := mux.Vars(r)["version"]
+
+	config, err := c.service.GetConfigGrup(name, version)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	resp, err := json.Marshal(config)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(resp)
+}
+
+func (c ConfigGroupHandler) AddGroup(w http.ResponseWriter, r *http.Request) {
+
+	var newConfigGroup model.ConfigurationGroup
+	err := json.NewDecoder(r.Body).Decode(&newConfigGroup)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = c.service.AddConfigGrup(newConfigGroup)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintln(w, "Configuration successfully added")
+}
